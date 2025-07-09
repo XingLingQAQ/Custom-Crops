fb6b84fa02ee97398ccc0bcef96acab747845db9
星凌 <92240364+XingLingQAQ@users.noreply.github.com>
2025-07-09 23:56:37 +0800

    Update build.gradle.kts

--- compatibility-itemsadder-r2/build.gradle.kts
+++ compatibility-itemsadder-r2/build.gradle.kts
@@ -1,13 +1,13 @@
 repositories {
     mavenCentral()
-    maven("https://maven.devs.beer/")
+    maven("https://www.matteodev.it/spigot/public/maven/")
     maven("https://repo.papermc.io/repository/maven-public/")
 }
 
 dependencies {
     compileOnly(project(":api"))
     compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
-    compileOnly("dev.lone:api-itemsadder:4.0.9")
+    compileOnly("dev.lone:api-itemsadder:4.0.10")
 }
 
 java {
